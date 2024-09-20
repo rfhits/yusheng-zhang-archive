201524ee58685e8efff839dca29c5d3f2c5f3a2b
rfhits <wysj0117@gmail.com>
2024-09-20 17:00:17 +0800

    fix: 测试使用中文语言包能否正确导出pdf

--- scripts/build_books.sh
+++ scripts/build_books.sh
@@ -5,6 +5,7 @@ sudo apt update
 sudo apt install libegl1
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 sudo apt-get install calibre
+sudo apt-get install -y language-pack-zh*
 
 mkdir -p ./_book/ebooks
 
@@ -14,11 +15,8 @@ bookname="yusheng-zhang-archive"
 # 创建目标目录
 mkdir -p ./_book/ebooks
 
-# 生成 EPUB 文件
 honkit epub ./ ./_book/ebooks/${bookname}.epub
 
-# 因为直接生成 pdf 文件无法显示汉字，所以从 epub 转 pdf
-ebook-convert ./_book/ebooks/${bookname}.epub ./_book/ebooks/${bookname}.pdf --output-profile=pdf --ignore-embedded-fonts
+honkit epub ./ ./_book/ebooks/${bookname}.pdf
 
-# 生成 MOBI 文件
 honkit mobi ./ ./_book/ebooks/${bookname}.mobi
