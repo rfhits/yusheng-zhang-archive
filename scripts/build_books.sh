eeea8cb786b62ac197bf975b59fda0f415565f54
rfhits <wysj0117@gmail.com>
2024-09-20 15:42:06 +0800

    fix: try to build ebooks

--- scripts/build_books.sh
+++ scripts/build_books.sh
@@ -3,21 +3,12 @@
 # install ebook-convert
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 
-# install honkit etc.
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-chmod +x "$SCRIPT_DIR/install_packages.sh"
-"$SCRIPT_DIR/install_packages.sh"
+# # install honkit etc.
+# SCRIPT_DIR=$(dirname "$(realpath "$0")")
+# chmod +x "$SCRIPT_DIR/install_packages.sh"
+# "$SCRIPT_DIR/install_packages.sh"
 
-# 定义目标目录
-targetDir="./_book/ebooks"
-
-# 检查目标目录是否存在
-if [ ! -d "$targetDir" ]; then
-    echo "Creating directory: $targetDir"
-    mkdir -p "$targetDir"  # 使用 -p 选项以确保创建父目录
-else
-    echo "Directory $targetDir already exists, skipping creation."
-fi
+mkdir ./_book/ebooks
 
 # 生成 PDF 文件
 honkit pdf ./_book/ebooks/yusheng-zhang-archive.pdf
