b4874a7d53a51952a0035900bafb1cc2e12e9742
rfhits <wysj0117@gmail.com>
2024-09-20 16:20:53 +0800

    fix: 添加电子书下载链接；尝试从 epub 到 pdf 修复无法显示汉字的问题

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Yusheng Zhang Archive
+
+# 电子书
+
+-   [epub](./ebooks/yusheng-zhang-archive.epub)
+-   [pdf](./ebooks/yusheng-zhang-archive.pdf)
+-   [mobi](./ebooks/yusheng-zhang-archive.mobi)
--- scripts/build_books.sh
+++ scripts/build_books.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
+
+# install ebook-convert and calibre
 sudo apt update
 sudo apt install libegl1
-
-# install ebook-convert
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
-sudo  apt-get update
 sudo apt-get install calibre
 
-# # install honkit etc.
-# SCRIPT_DIR=$(dirname "$(realpath "$0")")
-# chmod +x "$SCRIPT_DIR/install_packages.sh"
-# "$SCRIPT_DIR/install_packages.sh"
-
-
-pwd
 mkdir -p ./_book/ebooks
-echo "/books Created"
 
-# 生成 PDF 文件
-honkit pdf ./ ./_book/ebooks/yusheng-zhang-archive.pdf
+# 设置书籍名称变量
+bookname="yusheng-zhang-archive"
+
+# 创建目标目录
+mkdir -p ./_book/ebooks
 
 # 生成 EPUB 文件
-honkit epub ./ ./_book/ebooks/yusheng-zhang-archive.epub
+honkit epub ./ ./_book/ebooks/${bookname}.epub
+
+# 因为直接生成 pdf 文件无法显示汉字，所以从 epub 转 pdf
+ebook-convert ./_book/ebooks/${bookname}.epub ./_book/ebooks/${bookname}.pdf
 
 # 生成 MOBI 文件
-honkit mobi ./ ./_book/ebooks/yusheng-zhang-archive.mobi
+honkit mobi ./ ./_book/ebooks/${bookname}.mobi
