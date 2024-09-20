fa23b2e6af1603f786c98751cae3cce4c0f85e9f
rfhits <wysj0117@gmail.com>
2024-09-20 15:53:14 +0800

    fix: honkit 参数需要 build root

--- scripts/build_books.sh
+++ scripts/build_books.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt update
+sudo apt install libegl1
 
 # install ebook-convert
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
@@ -7,18 +9,17 @@ sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | 
 # SCRIPT_DIR=$(dirname "$(realpath "$0")")
 # chmod +x "$SCRIPT_DIR/install_packages.sh"
 # "$SCRIPT_DIR/install_packages.sh"
-sudo apt update
-sudo apt install libegl1
+
 
 pwd
 mkdir -p ./_book/ebooks
 echo "/books Created"
 
 # 生成 PDF 文件
-honkit pdf ./_book/ebooks/yusheng-zhang-archive.pdf
+honkit pdf ./ ./_book/ebooks/yusheng-zhang-archive.pdf
 
 # 生成 EPUB 文件
-honkit epub ./_book/ebooks/yusheng-zhang-archive.epub
+honkit epub ./ ./_book/ebooks/yusheng-zhang-archive.epub
 
 # 生成 MOBI 文件
-honkit mobi ./_book/ebooks/yusheng-zhang-archive.mobi
+honkit mobi ./ ./_book/ebooks/yusheng-zhang-archive.mobi
